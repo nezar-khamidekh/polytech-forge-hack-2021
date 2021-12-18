bce75c4b517f981f7643c26de46c0ed74afb1166
Damir564 <ddamir.nizamutdinov@yandex.ru>
2021-12-18 16:04:28 +0300

    Исправил команду прямоугольник с минусами

--- public/js/ForgeViewer.js
+++ public/js/ForgeViewer.js
@@ -53,11 +53,17 @@ const PLATE_COUNT = 100;
 $(document).ready(function () {
   getForgeToken(function (access_token) {
     jQuery.ajax({
-      url: "https://developer.api.autodesk.com/modelderivative/v2/designdata/" + urn + "/manifest",
+      url:
+        "https://developer.api.autodesk.com/modelderivative/v2/designdata/" +
+        urn +
+        "/manifest",
       headers: { Authorization: "Bearer " + access_token },
       success: function (res) {
         if (res.status === "success") launchViewer(urn);
-        else $("#forgeViewer").html("Преобразование всё ещё выполняется").css("color", "lightblue");
+        else
+          $("#forgeViewer")
+            .html("Преобразование всё ещё выполняется")
+            .css("color", "lightblue");
       },
     });
   });
@@ -71,12 +77,19 @@ function launchViewer(urn) {
   };
 
   Autodesk.Viewing.Initializer(options, () => {
-    viewer = new Autodesk.Viewing.GuiViewer3D(document.getElementById("forgeViewer"), {
-      extensions: [],
-    });
+    viewer = new Autodesk.Viewing.GuiViewer3D(
+      document.getElementById("forgeViewer"),
+      {
+        extensions: [],
+      }
+    );
     viewer.start();
     var documentId = "urn:" + urn;
-    Autodesk.Viewing.Document.load(documentId, onDocumentLoadSuccess, onDocumentLoadFailure);
+    Autodesk.Viewing.Document.load(
+      documentId,
+      onDocumentLoadSuccess,
+      onDocumentLoadFailure
+    );
   });
 }
 
@@ -84,14 +97,20 @@ function onDocumentLoadSuccess(doc) {
   var viewables = doc.getRoot().getDefaultGeometry();
   console.log(viewables);
   viewer.loadDocumentNode(doc, viewables).then((i) => {
-    viewer.addEventListener(Autodesk.Viewing.OBJECT_TREE_CREATED_EVENT, setupMyModel);
+    viewer.addEventListener(
+      Autodesk.Viewing.OBJECT_TREE_CREATED_EVENT,
+      setupMyModel
+    );
     // documented loaded, any action?
   });
 }
 
 function onDocumentLoadFailure(viewerErrorCode, viewerErrorMsg) {
   console.error(
-    "onDocumentLoadFailure() - errorCode:" + viewerErrorCode + "\n- errorMessage:" + viewerErrorMsg
+    "onDocumentLoadFailure() - errorCode:" +
+      viewerErrorCode +
+      "\n- errorMessage:" +
+      viewerErrorMsg
   );
 }
 
@@ -119,8 +138,11 @@ function setupMyModel() {
     new THREE.BoxGeometry(0, 0, 0),
     new THREE.MeshBasicMaterial({ color: 0xffffff, visible: false })
   );
-  const Position_BaseRod_Matrix = getFragmentWorldMatrixByNodeId(ID_BaseRod).matrix[0];
-  const Position_BaseRod = v.setFromMatrixPosition(Position_BaseRod_Matrix).clone();
+  const Position_BaseRod_Matrix =
+    getFragmentWorldMatrixByNodeId(ID_BaseRod).matrix[0];
+  const Position_BaseRod = v
+    .setFromMatrixPosition(Position_BaseRod_Matrix)
+    .clone();
 
   Pivot_BaseRod.position.x = Position_BaseRod.x;
   Pivot_BaseRod.position.y = Position_BaseRod.y;
@@ -131,14 +153,21 @@ function setupMyModel() {
     new THREE.BoxGeometry(0, 0, 0),
     new THREE.MeshBasicMaterial({ color: 0xffffff, visible: false })
   );
-  const Position_LowerArmBody_Matrix = getFragmentWorldMatrixByNodeId(ID_LowerArmBody).matrix[0];
-  const Position_LowerArmBody = v.setFromMatrixPosition(Position_LowerArmBody_Matrix).clone();
+  const Position_LowerArmBody_Matrix =
+    getFragmentWorldMatrixByNodeId(ID_LowerArmBody).matrix[0];
+  const Position_LowerArmBody = v
+    .setFromMatrixPosition(Position_LowerArmBody_Matrix)
+    .clone();
   Helper_LowerArmBody.position.x =
-    -Position_LowerArmBody.x + Math.abs(Position_LowerArmBody.x - Pivot_BaseRod.position.x);
+    -Position_LowerArmBody.x +
+    Math.abs(Position_LowerArmBody.x - Pivot_BaseRod.position.x);
   Helper_LowerArmBody.position.y =
-    -Position_LowerArmBody.y + Math.abs(Position_LowerArmBody.y - Pivot_BaseRod.position.y);
+    -Position_LowerArmBody.y +
+    Math.abs(Position_LowerArmBody.y - Pivot_BaseRod.position.y);
   Helper_LowerArmBody.position.z =
-    -Position_LowerArmBody.z + Math.abs(Position_LowerArmBody.z - Pivot_BaseRod.position.z) - 1700;
+    -Position_LowerArmBody.z +
+    Math.abs(Position_LowerArmBody.z - Pivot_BaseRod.position.z) -
+    1700;
   Pivot_BaseRod.add(Helper_LowerArmBody);
 
   // /* ====================== SecondAxis ================= */
@@ -148,12 +177,18 @@ function setupMyModel() {
     new THREE.MeshBasicMaterial({ color: 0xffffff, visible: false })
   );
 
-  const Position_LowerRodBody_Matrix = getFragmentWorldMatrixByNodeId(ID_LowerRodBody).matrix[0];
-  const Position_LowerRodBody = v.setFromMatrixPosition(Position_LowerRodBody_Matrix).clone();
+  const Position_LowerRodBody_Matrix =
+    getFragmentWorldMatrixByNodeId(ID_LowerRodBody).matrix[0];
+  const Position_LowerRodBody = v
+    .setFromMatrixPosition(Position_LowerRodBody_Matrix)
+    .clone();
 
-  Pivot_LowerRodBody.position.x = Position_LowerRodBody.x - Pivot_BaseRod.position.x;
-  Pivot_LowerRodBody.position.y = Position_LowerRodBody.y - Pivot_BaseRod.position.y;
-  Pivot_LowerRodBody.position.z = Position_LowerRodBody.z - Pivot_BaseRod.position.z;
+  Pivot_LowerRodBody.position.x =
+    Position_LowerRodBody.x - Pivot_BaseRod.position.x;
+  Pivot_LowerRodBody.position.y =
+    Position_LowerRodBody.y - Pivot_BaseRod.position.y;
+  Pivot_LowerRodBody.position.z =
+    Position_LowerRodBody.z - Pivot_BaseRod.position.z;
   Pivot_BaseRod.add(Pivot_LowerRodBody);
 
   Helper_LowerRodBody = new THREE.Mesh(
@@ -162,13 +197,25 @@ function setupMyModel() {
   );
   Helper_LowerRodBody.position.x =
     -Position_LowerRodBody.x +
-    Math.abs(Position_LowerRodBody.x - Pivot_LowerRodBody.position.x - Pivot_BaseRod.position.x);
+    Math.abs(
+      Position_LowerRodBody.x -
+        Pivot_LowerRodBody.position.x -
+        Pivot_BaseRod.position.x
+    );
   Helper_LowerRodBody.position.y =
     -Position_LowerRodBody.y +
-    Math.abs(Position_LowerRodBody.y - Pivot_LowerRodBody.position.y - Pivot_BaseRod.position.y);
+    Math.abs(
+      Position_LowerRodBody.y -
+        Pivot_LowerRodBody.position.y -
+        Pivot_BaseRod.position.y
+    );
   Helper_LowerRodBody.position.z =
     -Position_LowerRodBody.z +
-    Math.abs(Position_LowerRodBody.z - Pivot_LowerRodBody.position.z - Pivot_BaseRod.position.z);
+    Math.abs(
+      Position_LowerRodBody.z -
+        Pivot_LowerRodBody.position.z -
+        Pivot_BaseRod.position.z
+    );
   Pivot_LowerRodBody.add(Helper_LowerRodBody);
 
   Helper_MiddleArmBody = new THREE.Mesh(
@@ -176,18 +223,33 @@ function setupMyModel() {
     new THREE.MeshBasicMaterial({ color: 0xffffff, visible: false })
   );
 
-  const Position_MiddleArmBody_Matrix = getFragmentWorldMatrixByNodeId(ID_MiddleArmBody).matrix[0];
-  const Position_MiddleArmBody = v.setFromMatrixPosition(Position_MiddleArmBody_Matrix).clone();
+  const Position_MiddleArmBody_Matrix =
+    getFragmentWorldMatrixByNodeId(ID_MiddleArmBody).matrix[0];
+  const Position_MiddleArmBody = v
+    .setFromMatrixPosition(Position_MiddleArmBody_Matrix)
+    .clone();
 
   Helper_MiddleArmBody.position.x =
     -Position_MiddleArmBody.x +
-    Math.abs(Position_MiddleArmBody.x - Pivot_LowerRodBody.position.x - Pivot_BaseRod.position.x);
+    Math.abs(
+      Position_MiddleArmBody.x -
+        Pivot_LowerRodBody.position.x -
+        Pivot_BaseRod.position.x
+    );
   Helper_MiddleArmBody.position.y =
     -Position_MiddleArmBody.y +
-    Math.abs(Position_MiddleArmBody.y - Pivot_LowerRodBody.position.y - Pivot_BaseRod.position.y);
+    Math.abs(
+      Position_MiddleArmBody.y -
+        Pivot_LowerRodBody.position.y -
+        Pivot_BaseRod.position.y
+    );
   Helper_MiddleArmBody.position.z =
     -Position_MiddleArmBody.z +
-    Math.abs(Position_MiddleArmBody.z - Pivot_LowerRodBody.position.z - Pivot_BaseRod.position.z);
+    Math.abs(
+      Position_MiddleArmBody.z -
+        Pivot_LowerRodBody.position.z -
+        Pivot_BaseRod.position.z
+    );
   Pivot_LowerRodBody.add(Helper_MiddleArmBody);
 
   Pivot_UpperRodBody = new THREE.Mesh(
@@ -195,15 +257,24 @@ function setupMyModel() {
     new THREE.MeshBasicMaterial({ color: 0xffffff, visible: false })
   );
 
-  const Position_UpperRodBody_Matrix = getFragmentWorldMatrixByNodeId(ID_UpperRodBody).matrix[0];
-  const Position_UpperRodBody = v.setFromMatrixPosition(Position_UpperRodBody_Matrix).clone();
+  const Position_UpperRodBody_Matrix =
+    getFragmentWorldMatrixByNodeId(ID_UpperRodBody).matrix[0];
+  const Position_UpperRodBody = v
+    .setFromMatrixPosition(Position_UpperRodBody_Matrix)
+    .clone();
 
   Pivot_UpperRodBody.position.x =
-    Position_UpperRodBody.x - Pivot_LowerRodBody.position.x - Pivot_BaseRod.position.x;
+    Position_UpperRodBody.x -
+    Pivot_LowerRodBody.position.x -
+    Pivot_BaseRod.position.x;
   Pivot_UpperRodBody.position.y =
-    Position_UpperRodBody.y - Pivot_LowerRodBody.position.y - Pivot_BaseRod.position.y;
+    Position_UpperRodBody.y -
+    Pivot_LowerRodBody.position.y -
+    Pivot_BaseRod.position.y;
   Pivot_UpperRodBody.position.z =
-    Position_UpperRodBody.z - Pivot_LowerRodBody.position.z - Pivot_BaseRod.position.z;
+    Position_UpperRodBody.z -
+    Pivot_LowerRodBody.position.z -
+    Pivot_BaseRod.position.z;
   Pivot_LowerRodBody.add(Pivot_UpperRodBody);
 
   Helper_UpperRodBody = new THREE.Mesh(
@@ -242,8 +313,11 @@ function setupMyModel() {
     new THREE.MeshBasicMaterial({ color: 0xffffff, visible: false })
   );
 
-  const Position_UpperArmBody_Matrix = getFragmentWorldMatrixByNodeId(ID_UpperArmBody).matrix[0];
-  const Position_UpperArmBody = v.setFromMatrixPosition(Position_UpperArmBody_Matrix).clone();
+  const Position_UpperArmBody_Matrix =
+    getFragmentWorldMatrixByNodeId(ID_UpperArmBody).matrix[0];
+  const Position_UpperArmBody = v
+    .setFromMatrixPosition(Position_UpperArmBody_Matrix)
+    .clone();
 
   Helper_UpperArmBody.position.x =
     -Position_UpperArmBody.x +
@@ -278,8 +352,11 @@ function setupMyModel() {
     new THREE.MeshBasicMaterial({ color: 0xffffff, visible: false })
   );
 
-  const Position_HookBody_Matrix = getFragmentWorldMatrixByNodeId(ID_HookBody).matrix[0];
-  const Position_HookBody = v.setFromMatrixPosition(Position_HookBody_Matrix).clone();
+  const Position_HookBody_Matrix =
+    getFragmentWorldMatrixByNodeId(ID_HookBody).matrix[0];
+  const Position_HookBody = v
+    .setFromMatrixPosition(Position_HookBody_Matrix)
+    .clone();
 
   Pivot_HookBody.position.x =
     Position_HookBody.x -
@@ -348,7 +425,11 @@ function setupMyModel() {
     }
   }
 
-  plateGroup.position.set(-Helper_HookBody.position.x, -Helper_HookBody.position.y, -1000);
+  plateGroup.position.set(
+    -Helper_HookBody.position.x,
+    -Helper_HookBody.position.y,
+    -1000
+  );
 
   if (!viewer.overlays.hasScene("custom-scene")) {
     viewer.overlays.addScene("custom-scene");
@@ -485,7 +566,10 @@ function onSendCommands(actionCommands) {
   let commands;
   if (actionCommands) commands = actionCommands;
   else {
-    const textAreaValue = document.getElementById("commands").value.trim().replace(/\n/g, "");
+    const textAreaValue = document
+      .getElementById("commands")
+      .value.trim()
+      .replace(/\n/g, "");
     commands = textAreaValue.split(";").filter((v) => v !== "");
   }
 
@@ -505,46 +589,58 @@ function onSendCommands(actionCommands) {
 }
 
 function handleCommand(command, commandTitle) {
-  let commandValue = command.substring(command.indexOf("(") + 1, command.indexOf(")"));
+  let commandValue = command.substring(
+    command.indexOf("(") + 1,
+    command.indexOf(")")
+  );
   if (COMMANDS.includes(commandTitle)) {
     switch (commandTitle) {
       //ОСНОВАНИЕ
       case COMMANDS[0]:
         if (commandValue > MAIN_MAX_VALUE) commandValue = MAIN_MAX_VALUE;
         if (commandValue < MAIN_MIN_VALUE) commandValue = MAIN_MIN_VALUE;
-        updateWithAnimation(mainPartTranslate, commandValue / TRANSLATE_VALUE_DEVIDER);
+        updateWithAnimation(
+          mainPartTranslate,
+          commandValue / TRANSLATE_VALUE_DEVIDER
+        );
         break;
       //ТУЛОВИЩЕ
       case COMMANDS[1]:
         if (commandValue > BODY_MAX_VALUE) commandValue = BODY_MAX_VALUE;
         if (commandValue < BODY_MIN_VALUE) commandValue = BODY_MIN_VALUE;
-        updateWithAnimation(bodyPartTranslate, commandValue / TRANSLATE_VALUE_DEVIDER);
+        updateWithAnimation(
+          bodyPartTranslate,
+          commandValue / TRANSLATE_VALUE_DEVIDER
+        );
         break;
       //ГОЛОВА
       case COMMANDS[2]:
         if (commandValue > HEAD_MAX_VALUE) commandValue = HEAD_MAX_VALUE;
         if (commandValue < HEAD_MIN_VALUE) commandValue = HEAD_MIN_VALUE;
-        updateWithAnimation(headPartTranslate, commandValue / TRANSLATE_VALUE_DEVIDER);
+        updateWithAnimation(
+          headPartTranslate,
+          commandValue / TRANSLATE_VALUE_DEVIDER
+        );
         break;
       //ПРЯМОУГОЛЬНИК
       case COMMANDS[4]:
-        const length = commandValue.split(",")[0].trim();
-        const width = commandValue.split(",")[1].trim();
+        const length = commandValue.split(", ")[0].trim();
+        const width = commandValue.split(", ")[1].trim();
         const headRotatationRatio = width * 0.06;
         const commands = [
-          `ПОВОРОТ_ОСНОВАНИЕ(-${length / 2})`,
+          `ПОВОРОТ_ОСНОВАНИЕ(${-length / 2})`,
           "ПАУЗА()",
-          `ПОВОРОТ_ТУЛОВИЩЕ(-${width})`,
+          `ПОВОРОТ_ТУЛОВИЩЕ(${-width})`,
           "ПАУЗА()",
           `ПОВОРОТ_ОСНОВАНИЕ(${length / 2})`,
-          `ПОВОРОТ_ГОЛОВА(-${headRotatationRatio})`,
+          `ПОВОРОТ_ГОЛОВА(${-headRotatationRatio})`,
           "ПАУЗА()",
           `ПОВОРОТ_ОСНОВАНИЕ(${length / 2})`,
           `ПОВОРОТ_ГОЛОВА(${headRotatationRatio})`,
           "ПАУЗА()",
           `ПОВОРОТ_ТУЛОВИЩЕ(${width})`,
           "ПАУЗА()",
-          `ПОВОРОТ_ОСНОВАНИЕ(-${length / 2})`,
+          `ПОВОРОТ_ОСНОВАНИЕ(${-length / 2})`,
         ];
         onSendCommands(commands);
         break;
